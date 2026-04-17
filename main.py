791ca4a4eee96f5555ba89cc0bf0bdcb6ce54d8b
Willian <willianvi2024@gmail.com>
2026-04-16 22:03:13 -0300

    fisrt

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import random
+
+app = FastAPI()
+
+@app.get("/helloworld")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/funcaoteste")
+async def funcaoteste():
+    return {"teste": True, "num_aleatorio": random.randint(0, 20000)}
